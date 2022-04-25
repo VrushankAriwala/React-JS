20ac3d4531a90b0a63a240725d0933f28a7d9a1a
Smeet <patelsmeet0904@gmail.com>
2022-04-25 12:42:12 +0530

    added to do list app using react

---
+++ To-Do list/build/precache-manifest.bf1421afd1fb5afac65a2e6810b80e22.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c83dc8d5fa406dd4b7a9a3e0b967e77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a6035e60db09faea70f",
+    "url": "/static/js/2.3c779e7e.chunk.js"
+  },
+  {
+    "revision": "a8d36ca19b896fc4235a",
+    "url": "/static/js/main.9742f7bc.chunk.js"
+  },
+  {
+    "revision": "bb603cdcecd78fe2119f",
+    "url": "/static/js/runtime-main.2a02be55.js"
+  }
+]);
